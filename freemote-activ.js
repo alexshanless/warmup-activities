43b7b69a8bc902debb7971d7d6661b1a6e1f207c
alexshanless <alex.shanless@gmail.com>
2021-08-14 14:32:26 -0700

    first

---
+++ freemote-activ.js
@@ -0,0 +1,9 @@
+// Create a string to get initials from a first and last name.
+
+// Must be uppercase and separated with a period (".")
+
+var getInitials = function (name) {
+  // your code here
+};
+
+getInitials("harry potter");
