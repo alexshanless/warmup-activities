c8c87bdb563bb894682ea2a70819face087e47d5
alexshanless <alex.shanless@gmail.com>
2021-08-14 21:27:26 -0700

    10

--- freemote-activ.js
+++ freemote-activ.js
@@ -2,8 +2,93 @@
 
 // Must be uppercase and separated with a period (".")
 
-var getInitials = function (name) {
-  // your code here
+// var getInitials = function (name) {
+//   // your code here
+// };
+
+// console.log(getInitials("harry potter"));
+
+// Given an array of numbers, add together the positive nums & return the sum
+
+// If no numbers are positive, return 0
+
+// var sumOfPositive = (nums) => {};
+
+// console.log(sumOfPositive([1, -2, 3, 4]));
+
+// ✖️ multiplyNumbers
+// Given 2 numbers, multiply them WITHOUT THE * OPERATOR!
+var multiplyNumbers = function (num1, num2) {
+  let sum = 0;
+  for (let i = 0; i < num2; i++) {
+    sum += num1;
+  }
+  return sum;
 };
 
-getInitials("harry potter");
+// Given a string, duplicate in n times and return a single string
+
+var repeatString = function (s, n) {
+  let str = "";
+  // your code here
+  for (i = 0; i < n; i++) {
+    str += s;
+  }
+  return str;
+};
+
+// Given a string, determine whether it is a palindrome (same forwards and backwards, case-sensitive)
+
+var isPalindrome = function (str) {
+  // your code here
+  let newStr = str.split("").reverse().join("");
+  if (newStr === str) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//Given a number, count how many odd numbers are below that number
+
+var countOdd = function (n) {
+  // your code here
+  Math.floor(n / 2);
+};
+
+// Given a number, count how many numbers below it are multiples of 3 and 5
+
+// Then, sum those multiples together
+
+var sumMultiples = function (n) {
+  var counter = 0;
+  for (let i = 0; i < n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      counter += i;
+    }
+  }
+
+  return counter;
+};
+
+// Given an array of positive and negative integers, find the number that does not have an opposite
+var unbalancedArray = function (nums) {
+  return nums.reduce((sum, num) => sum + num);
+};
+
+// Given a string, count the vowels and return that count
+var countVowels = function (s) {
+  var counter = 0;
+  for (var char of s) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      counter++;
+    }
+  }
+  return counter;
+};
