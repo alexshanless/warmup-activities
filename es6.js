188af01197ea8a6c2978fbe0e45311fca112d033
alexshanless <alex.shanless@gmail.com>
2022-01-10 19:57:29 -0800

    es6

---
+++ es6.js
@@ -0,0 +1,33 @@
+//Let  + const
+let a = 24;
+if (true) {
+  let a = 42;
+}
+
+//Hoisting
+
+// b = 20;
+
+// console.log(b);
+
+// let b = 10;
+
+function hoisted() {
+  age = 29;
+}
+
+let age;
+hoisted();
+
+//Const
+const COLOR = "ffaaee";
+// COLOR = "000000";
+
+const array = [1, 1, 2, 3, 5, 8];
+array.push(13);
+
+const obj = { a: 42 };
+obj.name = "Antoine";
+console.log(obj);
+
+//ARROW FUNC
