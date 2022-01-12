52a9069f7e22442dde48cc61b29deb7943bb3fb1
alexshanless <alex.shanless@gmail.com>
2022-01-11 18:58:12 -0800

    REST SPREAD!

--- es6.js
+++ es6.js
@@ -49,8 +49,60 @@ const person = {
   log: log,
   arrowLog: arrowLog,
   delayLog: function () {
-    setTimeout(() => console.log(this.name + " " + this.age), 500);
+    setTimeout(() => this.name + " " + this.age, 500);
   },
 };
 
 person.delayLog();
+
+//DEFAULT PARAMS
+const getDefault = (c) => c * 2;
+
+function compute(a = 10, b = getDefault(a)) {
+  return a + b;
+}
+
+//Strings
+
+const title = "Hello";
+
+const isVisible = () => Math.random > 0.5;
+
+const template = `
+${isVisible() ? `<p>Visible<p>` : ""}
+<h1 style='color: blue'>${title}<h1>
+`;
+
+// console.log(template);
+
+//Methods
+const str = "Hello";
+
+// console.log(str.startsWith("H"));
+// console.log(str.endsWith("o"));
+// console.log(str.trimEnd());
+// console.log(str.padStart(10));
+
+//REST SPREAD
+// function average(arr) {
+//   return arr.reduce((acc, i) => (acc += i), 0) / arr.length;
+// }
+
+// console.log(average([10, 20, 30, 40]));
+
+// const array = [1, 2, 3, 5, 8];
+// const [a, , c] = array;
+
+//Objects
+const address = {
+  country: "USA",
+  city: "Seattle",
+  street: "Broadway",
+  concat: function () {
+    return `${this.contry}, ${this.city}, ${this.street}`;
+  },
+};
+
+// const { city, country, street, concat: addressConcat } = address;
+
+const { city, ...rest } = address;
